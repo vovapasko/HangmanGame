a2972b622550a5cd3dc48c9a00f98a32c52ce181
vovapasko <w.pasko@neurologiq.de>
2022-01-14 19:07:21 +0100

    Initial Commit

---
+++ HangmanGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HangmanGame
+//
+//  Created by Wladimir Pasko on 14.01.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

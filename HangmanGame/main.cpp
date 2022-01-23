afe19dd3ac8cda1d4426e665f70fba4969169b87
vovapasko <vovapasko1699@gmail.com>
2022-01-23 15:18:04 +0100

    finished base functionality of the game

--- HangmanGame/main.cpp
+++ HangmanGame/main.cpp
@@ -33,29 +33,39 @@ int main() {
 }
 
 
+void generate_hidden_word(std::string &hidden_word, const std::string &word) {
+    for (int i = 0; i < word.size(); i++) {
+        hidden_word.insert(i, "-");
+    }
+}
+
+void show_hidden_word(const std::string &hidden_word) {
+    cout << "Here is how the word looks like: ";
+    cout << hidden_word << endl;
+}
+
 void start_game(string word){
     string hidden_word;
     char users_letter;
     bool game_finished = false;
     bool word_guessed;
-    cout << "Now let's start guessing. Here is how the word looks like: ";
-    for (int i = 0; i < word.size(); i++) {
-        hidden_word.insert(i, "-");
-    }
-    cout << hidden_word << endl;
+    cout << "Let's start the game" << endl;
+    generate_hidden_word(hidden_word, word);
+    show_hidden_word(hidden_word);
     display_attemts();
     while (game_finished == false) {
         cout << "Type the letter: ";
         cin >> users_letter;
-        cout << users_letter;
         auto indices = find_letter_index(word, hidden_word, users_letter);
         if (indices.size() > 0){
             open_letters_in_word(word, hidden_word, indices);
         }
         else{
+            cout << "You are wrong. ";
             cout << "Your letter is not in the word" << endl;
         }
         max_tries--;
+        show_hidden_word(hidden_word);
         display_attemts();
         word_guessed = check_word_status(hidden_word);
         if (word_guessed) {
@@ -86,7 +96,7 @@ void open_letters_in_word(string origin_word, string &hidden_word, vector<int> i
 }
 
 void display_attemts(){
-    cout << "You have " << max_tries << " attempts";
+    cout << "You have " << max_tries << " attempts" << endl;
 }
 
 vector<int> find_letter_index(string origin_word, string& hidden_word, char users_letter){
@@ -97,14 +107,13 @@ vector<int> find_letter_index(string origin_word, string& hidden_word, char user
         }
     }
     return letter_indicies;
-    
 }
 
 
 string chose_the_word(){
-    string words[] = {"dog", "parrot", "kitten"};
+    string word_bank[] = {"dog", "parrot", "kitten"};
     int word_index = pick_random_number(0, 3);
-    return words[word_index];
+    return word_bank[word_index];
 }
 
 int pick_random_number(int min, int max){
